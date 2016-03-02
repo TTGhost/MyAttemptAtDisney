733f2a87472049c4aeb86cb6218458f28ddff54b
TTGhost <TTGhost@users.noreply.github.com>
2016-03-01 21:38:32 -0600

    Create __init__.py

---
+++ libtoontown/__init__.py
@@ -0,0 +1,25 @@
+## REFERENCE ##
+"""
+FUNCTIONS
+    Dtool_AddToDictionary(...)
+        Used to Items Into a types (tp_dict)
+    
+    Dtool_BorrowThisReference(...)
+        Used to borrow 'this' poiner ( to, from)
+        Assumes no ownership
+    
+    getDnaPath(...)
+    
+    loadDNAFile(...)
+    
+    loadDNAFileAI(...)
+
+DATA
+    CSDefault = 0
+    CSInvalid = 5
+    CSYupLeft = 4
+    CSYupRight = 2
+    CSZupLeft = 3
+    CSZupRight = 1
+    Dtool_PyNativeInterface = 1
+"""
