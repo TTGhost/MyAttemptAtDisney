ddcb7fd5765abb706099ae1609e66ef2125b56f5
TTGhost <TTGhost@users.noreply.github.com>
2016-03-01 21:39:52 -0600

    Create DNAVisGroup.py

---
+++ libtoontown/DNAVisGroup.py
@@ -0,0 +1,246 @@
+
+class DNAVisGroup:
+
+    def __init__(self):
+        pass
+
+    def addBattleCell(self):
+        pass
+
+    def addSuitEdge(self):
+        pass
+
+    def addVisible(self):
+        pass
+
+    def getBattleCell(self):
+        pass
+
+    def getNumBattleCells(self):
+        pass
+
+    def getNumSuitEdges(self):
+        pass
+
+    def getNumBattleCells(self):
+        pass
+
+    def getNumVisibles(self):
+        pass
+
+    def getSuitEdge(self):
+        pass
+
+    def getVisibleName(self):
+        pass
+
+    def removeBattleCell(self):
+        pass
+
+    def removeSuitEdge(self):
+        pass
+
+    def removeVisible(self):
+        pass
+
+## REFERENCE ##
+"""
+    class DNAVisGroup(DNAGroup)
+     |  Method resolution order:
+     |      DNAVisGroup
+     |      DNAGroup
+     |      libpandaexpress.TypedReferenceCount
+     |      libpandaexpress.TypedObject
+     |      libpandaexpress.ReferenceCount
+     |      libpandaexpress.Namable
+     |      libdtoolconfig.DTOOL_SUPPER_BASE111
+     |      __builtin__.object
+     |  
+     |  Methods defined here:
+     |  
+     |  __copy__(...)
+     |  
+     |  __deepcopy__(...)
+     |  
+     |  __delattr__(...)
+     |      x.__delattr__('name') <==> del x.name
+     |  
+     |  __getattribute__(...)
+     |      x.__getattribute__('name') <==> x.name
+     |  
+     |  __init__(...)
+     |      x.__init__(...) initializes x; see x.__class__.__doc__ for signature
+     |  
+     |  __setattr__(...)
+     |      x.__setattr__('name', value) <==> x.name = value
+     |  
+     |  addBattleCell(...)
+     |  
+     |  addSuitEdge(...)
+     |  
+     |  addVisible(...)
+     |  
+     |  getBattleCell(...)
+     |  
+     |  getNumBattleCells(...)
+     |  
+     |  getNumSuitEdges(...)
+     |  
+     |  getNumVisibles(...)
+     |  
+     |  getSuitEdge(...)
+     |  
+     |  getVisibleName(...)
+     |  
+     |  removeBattleCell(...)
+     |  
+     |  removeSuitEdge(...)
+     |  
+     |  removeVisible(...)
+     |  
+     |  ----------------------------------------------------------------------
+     |  Data and other attributes defined here:
+     |  
+     |  DNAVisGroup = <type 'libtoontown.DNAVisGroup'>
+     |  
+     |  
+     |  DtoolClassDict = {'DNAVisGroup': <type 'libtoontown.DNAVisGroup'>, 'Dt...
+     |  
+     |  __new__ = <built-in method __new__ of type object>
+     |      T.__new__(S, ...) -> a new object with type S, a subtype of T
+     |  
+     |  getClassType = <built-in method getClassType of type object>
+     |  
+     |  
+     |  this = <member 'this' of 'libtoontown.DNAVisGroup' objects>
+     |      C++ This if any
+     |  
+     |  this_metatype = <member 'this_metatype' of 'libtoontown.DNAVisGroup' o...
+     |      The dtool meta object
+     |  
+     |  ----------------------------------------------------------------------
+     |  Methods inherited from DNAGroup:
+     |  
+     |  add(...)
+     |  
+     |  at(...)
+     |  
+     |  current(...)
+     |  
+     |  getNumChildren(...)
+     |  
+     |  getParent(...)
+     |  
+     |  ls(...)
+     |  
+     |  remove(...)
+     |  
+     |  topLevelTraverse(...)
+     |  
+     |  traverse(...)
+     |  
+     |  upcastToNamable(...)
+     |  
+     |  upcastToTypedReferenceCount(...)
+     |  
+     |  write(...)
+     |  
+     |  ----------------------------------------------------------------------
+     |  Data and other attributes inherited from DNAGroup:
+     |  
+     |  DNAGroup = <type 'libtoontown.DNAGroup'>
+     |  
+     |  
+     |  ----------------------------------------------------------------------
+     |  Methods inherited from libpandaexpress.TypedReferenceCount:
+     |  
+     |  upcastToReferenceCount(...)
+     |  
+     |  upcastToTypedObject(...)
+     |  
+     |  ----------------------------------------------------------------------
+     |  Data and other attributes inherited from libpandaexpress.TypedReferenceCount:
+     |  
+     |  TypedReferenceCount = <type 'libpandaexpress.TypedReferenceCount'>
+     |  
+     |  
+     |  ----------------------------------------------------------------------
+     |  Methods inherited from libpandaexpress.TypedObject:
+     |  
+     |  downcastToTypedReferenceCount(...)
+     |  
+     |  getType(...)
+     |  
+     |  getTypeIndex(...)
+     |  
+     |  isExactType(...)
+     |  
+     |  isOfType(...)
+     |  
+     |  ----------------------------------------------------------------------
+     |  Data and other attributes inherited from libpandaexpress.TypedObject:
+     |  
+     |  TypedObject = <type 'libpandaexpress.TypedObject'>
+     |  
+     |  
+     |  ----------------------------------------------------------------------
+     |  Methods inherited from libpandaexpress.ReferenceCount:
+     |  
+     |  getRefCount(...)
+     |  
+     |  ref(...)
+     |  
+     |  testRefCountIntegrity(...)
+     |  
+     |  testRefCountNonzero(...)
+     |  
+     |  unref(...)
+     |  
+     |  ----------------------------------------------------------------------
+     |  Data and other attributes inherited from libpandaexpress.ReferenceCount:
+     |  
+     |  ReferenceCount = <type 'libpandaexpress.ReferenceCount'>
+     |  
+     |  
+     |  ----------------------------------------------------------------------
+     |  Methods inherited from libpandaexpress.Namable:
+     |  
+     |  __repr__(...)
+     |      x.__repr__() <==> repr(x)
+     |  
+     |  __str__(...)
+     |      x.__str__() <==> str(x)
+     |  
+     |  assign(...)
+     |  
+     |  clearName(...)
+     |  
+     |  getName(...)
+     |  
+     |  hasName(...)
+     |  
+     |  output(...)
+     |  
+     |  setName(...)
+     |  
+     |  ----------------------------------------------------------------------
+     |  Data and other attributes inherited from libpandaexpress.Namable:
+     |  
+     |  Namable = <type 'libpandaexpress.Namable'>
+     |  
+     |  
+     |  ----------------------------------------------------------------------
+     |  Methods inherited from libdtoolconfig.DTOOL_SUPPER_BASE111:
+     |  
+     |  __cmp__(...)
+     |      x.__cmp__(y) <==> cmp(x,y)
+     |  
+     |  __hash__(...)
+     |      x.__hash__() <==> hash(x)
+     |  
+     |  ----------------------------------------------------------------------
+     |  Data and other attributes inherited from libdtoolconfig.DTOOL_SUPPER_BASE111:
+     |  
+     |  DtoolGetSupperBase = <built-in method DtoolGetSupperBase of type objec...
+     |      Will Return SUPPERbase Class
+"""
