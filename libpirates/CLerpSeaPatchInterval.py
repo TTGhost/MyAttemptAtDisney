3549df670139f24d63ef7156e64774edb58adffe
TTGhost <TTGhost@users.noreply.github.com>
2016-03-01 19:05:58 -0600

    upload some files / initial commit

---
+++ libpirates/CLerpSeaPatchInterval.py
@@ -0,0 +1,15 @@
+
+class CLerpSeaPatchInterval(CLerpInterval):
+    
+    def __init__(self):
+        pass
+
+    def getInitial(self):
+        pass
+
+    def getRoot(self):
+        pass
+
+    def getTarget(self):
+        pass
+
